5aa364a415e27efe330a9f19f60f3ae6da7ade20
Kirill <kirillkulakow31@gmail.com>
2020-03-17 22:49:37 +0200

    fix problems

--- src/index.js
+++ src/index.js
@@ -7,45 +7,43 @@ import menuLi from './menu-li.hbs'
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
-}
+};
 
-document.querySelector('body').classList.add(localStorage.getItem('theme'))
+document.querySelector('body').classList.add(localStorage.getItem('theme'));
 
 let switcher = document.querySelector('#theme-switch-control');
-let controlSwitchCheck = () => {
-    switch (document.querySelector('body').classList.contains(Theme.DARK)) {
-        case true:
-            switcher.setAttribute('checked', 'true');
-            break;
-        case false:
-            switcher.removeAttribute('checked')
-            break;
-    }
-}
-let saveTheme = (localTheme, classTheme) => {
-    localStorage.clear()
-    localStorage.setItem('theme', localTheme)
-    document.querySelector('body').classList.add(localStorage.getItem('theme'))
+
+const controlSwitchCheck = () => {
+    if (document.querySelector('body').classList.contains(Theme.DARK)) {
+            return switcher.setAttribute('checked', 'true');}
+        switcher.removeAttribute('checked');
+};
+
+const saveTheme = (localTheme, classTheme) => {
+    localStorage.clear();
+    localStorage.setItem('theme', localTheme);
+    document.querySelector('body').classList.add(localStorage.getItem('theme'));
     if (document.querySelector('body').classList.contains(classTheme)) {
-        document.querySelector('body').classList.remove(classTheme)
-    }
+        document.querySelector('body').classList.remove(classTheme);
+    };
     if (localTheme.includes('dark')){
-        switcher.setAttribute('checked', 'true')}
-    switcher.removeAttribute('checked')
-    ;
-}
-let switchFunction = (e) => {
+        return switcher.setAttribute('checked', 'true');
+    };
+    switcher.removeAttribute('checked');
+};
+
+const switchFunction = (e) => {
     if (e.target.checked) {
-        saveTheme('dark-theme', Theme.LIGHT)
-    } else {
-        saveTheme('light-theme', Theme.DARK)
-    }
-}
+       return saveTheme('dark-theme', Theme.LIGHT);
+    };
+    saveTheme('light-theme', Theme.DARK);
+};
+
 controlSwitchCheck();
-const eventSwitch = document.querySelector('.switch')
-eventSwitch.addEventListener('change', switchFunction)
+const eventSwitch = document.querySelector('.switch');
+eventSwitch.addEventListener('change', switchFunction);
 
 // Шаблонизация
 
-let ulList = document.querySelector('.js-menu')
-ulList.insertAdjacentHTML('afterbegin', menuLi(menuList))
+let ulList = document.querySelector('.js-menu');
+ulList.insertAdjacentHTML('afterbegin', menuLi(menuList));
